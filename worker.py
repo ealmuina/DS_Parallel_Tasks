5fde839f457954ae42e67ea79e075b87e25624b4
Eddy Almuiña <almuina.e@gmail.com>
2016-12-25 15:54:09 -0500

    Fixed avg_time formula on workers get_load function

--- worker.py
+++ worker.py
@@ -54,7 +54,7 @@ class Worker(Node):
          de completar y el tiempo promedio que demora en completar una."""
 
         total_time = self.total_time.total_seconds()
-        avg_time = self.total_operations / total_time if total_time > 0 else 1
+        avg_time = total_time / self.total_operations if total_time != 0 else 1
         return (self.load + 1) * avg_time
 
     def get_ip(self):
